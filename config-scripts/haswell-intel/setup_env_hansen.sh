ec3b18ce18f4bab25c2d1fb2720265c005ccda2a
Eric Phipps <etphipp@sandia.gov>
2017-07-28 10:09:48 -0600

    Example config script for inline build of Kokkos for Haswell.

--- config-scripts/haswell-intel/setup_env_hansen.sh
+++ config-scripts/haswell-intel/setup_env_hansen.sh
@@ -1,3 +1,6 @@
 module load intel/compilers/17.0.098
 module load cmake/3.4.3
 module load boost/1.59.0/openmpi/1.10.2/intel/17.0.042
+export OMP_NUM_THREADS=64
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
--- config-scripts/haswell-intel/setup_env_kahuna.sh
+++ config-scripts/haswell-intel/setup_env_kahuna.sh
@@ -2,3 +2,6 @@ module load intel/17.0.2
 module load intel-mkl/2017.2.174
 module load cmake/3.7.2
 module load boost/1.63.0
+export OMP_NUM_THREADS=64
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
