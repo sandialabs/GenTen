faf794ba879c80ec8f6df5e513c844ced7eabcab
Eric Phipps <etphipp@sandia.gov>
2020-03-03 17:53:16 -0700

    Add more sptensor_gt methods and expose Genten innerprod to matlab.

---
+++ matlab/Genten_Matlab_Innerprod.cpp
@@ -0,0 +1,87 @@
+//@HEADER
+// ************************************************************************
+//     Genten: Software for Generalized Tensor Decompositions
+//     by Sandia National Laboratories
+//
+// Sandia National Laboratories is a multimission laboratory managed
+// and operated by National Technology and Engineering Solutions of Sandia,
+// LLC, a wholly owned subsidiary of Honeywell International, Inc., for the
+// U.S. Department of Energy's National Nuclear Security Administration under
+// contract DE-NA0003525.
+//
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ************************************************************************
+//@HEADER
+
+// Mex Driver for computing permutation arrays
+
+#include "Genten_Matlab.hpp"
+#include "Genten_MixedFormatOps.hpp"
+#include "Genten_SystemTimer.hpp"
+
+extern "C" {
+
+DLL_EXPORT_SYM void mexFunction(int nlhs, mxArray *plhs[],
+                                int nrhs, const mxArray *prhs[])
+{
+  typedef Genten::DefaultExecutionSpace ExecSpace;
+  typedef Genten::SptensorT<ExecSpace> Sptensor_type;
+  typedef Genten::KtensorT<ExecSpace> Ktensor_type;
+  typedef Genten::FacMatrixT<ExecSpace> FacMatrix_type;
+  typedef Genten::Sptensor Sptensor_host_type;
+  typedef typename Sptensor_type::subs_view_type subs_type;
+  typedef typename Sptensor_host_type::subs_view_type host_subs_type;
+
+  GentenInitialize();
+
+  try {
+    if (nrhs != 2 || nlhs != 1) {
+      std::cout << "Expected exactly 2 input and 1 output arguments"
+                << std::endl;
+      return;
+    }
+
+    // Parse inputs
+    Sptensor_type X = mxGetSptensor<ExecSpace>(prhs[0]);
+    Ktensor_type u = mxGetKtensor<ExecSpace>(prhs[1]);
+
+    // Perform innerprod
+    ttb_real v = Genten::innerprod(X, u);
+
+    // Set output
+    plhs[0] = mxCreateDoubleScalar(static_cast<double>(v));
+  }
+
+  catch(std::string sExc)
+  {
+    std::cout << "*** Call to genten threw an exception:\n";
+    std::cout << "  " << sExc << "\n";
+  }
+}
+
+}
