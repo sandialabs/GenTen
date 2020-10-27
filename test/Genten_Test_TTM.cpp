ee8ac20552825a3365f554c2f3c1421e4535b140
ben-cobb <bcobb33@gatech.edu>
2020-10-27 01:34:33 -0400

    Added header statement and cleaned unit test

--- test/Genten_Test_TTM.cpp
+++ test/Genten_Test_TTM.cpp
@@ -1,4 +1,43 @@
-    // #include <gtest/gtest.h> 
+//@HEADER
+// ************************************************************************
+//     Genten: Software for Generalized Tensor Decompositions
+//     by Sandia National Laboratories
+//
+// Sandia National Laboratories is a multimission laboratory managed
+// and operated by National Technology and Engineering Solutions of Sandia,
+// LLC, a wholly owned subsidiary of Honeywell International, Inc., for the
+// U.S. Department of Energy's National Nuclear Security Administration under
+// contract DE-NA0003525.
+//
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ************************************************************************
+//@HEADER
+
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -17,8 +56,6 @@ bool unit_test_tensor(Genten::TensorT<Space> Z, ttb_real *unit_test, int prod)
 {
     for (int i = 0; i < prod; ++i)
     {
-        // std::cout << Z.getValues().values()(i) <<std::endl;
-        // EXPECT_NEAR(Z.getValues().values()(i), unit_test[i], 1e-10);
         if(Z.getValues().values()(i) != unit_test[i]){
             std::stringstream dim_error;
             dim_error << "Z[" << i << "]: " << Z.getValues().values()(i) << "   unit_test: " << unit_test[i];
@@ -45,7 +82,7 @@ int bulk_test(Genten::TensorT<Space> X, Genten::TensorT<Space> mat, int mode, tt
 
     Genten::AlgParams al;
 
-    MESSAGE("Testing default DGEMM cuBlas enabled ttm along mode: " + std::to_string(mode)); // std::strcat("along mode: ", std::to_string(mode).c_str())); //std::to_string(mode)
+    MESSAGE("Testing default DGEMM cuBlas enabled ttm along mode: " + std::to_string(mode)); 
 #if defined(KOKKOS_ENABLE_CUDA) && defined(HAVE_CUBLAS)
     Genten::TensorT<Genten::DefaultExecutionSpace> X_device = create_mirror_view(Genten::DefaultExecutionSpace(), X);
 	deep_copy(X_device, X);
@@ -75,7 +112,7 @@ int bulk_test(Genten::TensorT<Space> X, Genten::TensorT<Space> mat, int mode, tt
     Genten::Impl::genten_ttm_serial_dgemm(mode, X, mat, Z);
     ASSERT(unit_test_tensor(Z, unit_test, prod), "serial_dgemm");
     MESSAGE("Testing parfor dgemm along mode: " + std::to_string(mode));
-    //Z.getValues().values()(0)=483294;
+    // Z.getValues().values()(0)=483294;  //Sanity check
     Genten::Impl::genten_ttm_parfor_dgemm(mode, X, mat, Z);
     // Z.getValues().values()(0)=483294; //Sanity check
     ASSERT(unit_test_tensor(Z, unit_test, prod), "parfor_dgemm");
@@ -86,7 +123,6 @@ int bulk_test(Genten::TensorT<Space> X, Genten::TensorT<Space> mat, int mode, tt
     return 0;
 }
 
-// TEST(Test, ttm_test_0)
 void test0()
 {
     typedef Genten::DefaultHostExecutionSpace Space;
@@ -125,17 +161,19 @@ void test0()
     result_size[mode] = mat.size(0);
 
     Tensor_type Z(result_size, 0.0);
-    // std::cout << "preparing to calculate ttm:  5x2 * 3x4x2x2 along mode 0" << std::endl;
+   
     MESSAGE("preparing to calculate ttm:  5x2 * 3x4x2x2 along mode 0");
 
     int prod = Z.size().prod();
 
-    ttb_real unit_test[120] = {25, 28, 31, 34, 37, 70, 82, 94, 106, 118, 115, 136, 157, 178, 199, 160, 190, 220, 250, 280, 205, 244, 283, 322, 361, 250, 298, 346, 394, 442, 295, 352, 409, 466, 523, 340, 406, 472, 538, 604, 385, 460, 535, 610, 685, 430, 514, 598, 682, 766, 475, 568, 661, 754, 847, 520, 622, 724, 826, 928, 565, 676, 787, 898, 1009, 610, 730, 850, 970, 1090, 655, 784, 913, 1042, 1171, 700, 838, 976, 1114, 1252};
-    
+    ttb_real unit_test[120] = {25, 28, 31, 34, 37, 70, 82, 94, 106, 118, 115, 136, 157, 178, 199, 160, 190, 220, 250, 280, 205, 244, 283,
+322, 361, 250, 298, 346, 394, 442, 295, 352, 409, 466, 523, 340, 406, 472, 538, 604, 385, 460, 535, 610, 685, 430, 514, 598, 682, 766, 475,
+568, 661, 754, 847, 520, 622, 724, 826, 928, 565, 676, 787, 898, 1009, 610, 730, 850, 970, 1090, 655, 784, 913, 1042, 1171, 700, 838, 976,
+1114, 1252};
+
     bulk_test( X,  mat, mode, unit_test);
 }
 
-// TEST(Test, ttm_test_1)
 void test1()
 {
     typedef Genten::DefaultHostExecutionSpace Space;
@@ -178,13 +216,13 @@ void test1()
 
     int prod = Z.size().prod();
 
-    ttb_real unit_test[60] = {210, 240, 270, 228, 262, 296, 246, 284, 322, 264, 306, 348, 282, 328, 374, 570, 600, 630, 636, 670, 704, 702, 740, 778, 768, 810, 852, 834, 880, 926, 930, 960, 990, 1044, 1078, 1112, 1158, 1196, 1234, 1272, 1314, 1356, 1386, 1432, 1478, 1290, 1320, 1350, 1452, 1486, 1520, 1614, 1652, 1690, 1776, 1818, 1860, 1938, 1984, 2030};
-
+    ttb_real unit_test[60] = {210, 240, 270, 228, 262, 296, 246, 284, 322, 264, 306, 348, 282, 328, 374, 570, 600, 630, 636, 670, 704, 702,
+740, 778, 768, 810, 852, 834, 880, 926, 930, 960, 990, 1044, 1078, 1112, 1158, 1196, 1234, 1272, 1314, 1356, 1386, 1432, 1478, 1290, 1320,
+1350, 1452, 1486, 1520, 1614, 1652, 1690, 1776, 1818, 1860, 1938, 1984, 2030};
 
     bulk_test( X,  mat, mode, unit_test);
 }
 
-// TEST(Test, ttm_test_2)
 void test2()
 {
     typedef Genten::DefaultHostExecutionSpace Space;
@@ -227,13 +265,15 @@ void test2()
 
     int prod = Z.size().prod();
 
-    ttb_real unit_test[120] = {60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 72, 79, 86, 93, 100, 107, 114, 121, 128, 135, 142, 149, 84, 93, 102, 111, 120, 129, 138, 147, 156, 165, 174, 183, 96, 107, 118, 129, 140, 151, 162, 173, 184, 195, 206, 217, 108, 121, 134, 147, 160, 173, 186, 199, 212, 225, 238, 251, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 247, 254, 261, 268, 275, 282, 289, 296, 303, 310, 317, 300, 309, 318, 327, 336, 345, 354, 363, 372, 381, 390, 399, 360, 371, 382, 393, 404, 415, 426, 437, 448, 459, 470, 481, 420, 433, 446, 459, 472, 485, 498, 511, 524, 537, 550, 563};
-
+    ttb_real unit_test[120] = {60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 72, 79, 86, 93, 100, 107, 114, 121, 128, 135, 142, 149,
+84, 93, 102, 111, 120, 129, 138, 147, 156, 165, 174, 183, 96, 107, 118, 129, 140, 151, 162, 173, 184, 195, 206, 217, 108, 121, 134, 147,
+160, 173, 186, 199, 212, 225, 238, 251, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 247, 254, 261, 268, 275, 282, 289,
+296, 303, 310, 317, 300, 309, 318, 327, 336, 345, 354, 363, 372, 381, 390, 399, 360, 371, 382, 393, 404, 415, 426, 437, 448, 459, 470, 481,
+420, 433, 446, 459, 472, 485, 498, 511, 524, 537, 550, 563};
 
     bulk_test( X,  mat, mode, unit_test);
 }
 
-// TEST(Test, ttm_test_3)
 void test3()
 {
     typedef Genten::DefaultHostExecutionSpace Space;
@@ -284,13 +324,6 @@ void test3()
                                398, 411, 424, 437, 450, 463, 476, 489, 502, 515};
 
     bulk_test( X,  mat, mode, unit_test);
-
-    // matrix_dims[1] = 3;
-    // Tensor_type mat2(matrix_dims, 0.0);
-    // Genten::Impl::genten_ttm_parfor_dgemm(4, X, mat, Z);
-    // Genten::Impl::genten_ttm_serial_dgemm(1, X, mat, Z);
-    // FacMatrix_type facmat(mat.size(0),mat.size(1),mat.getValues().values().data());
-    // bulk_test( X, facmat, mode, unit_test);
 }
 
 void Genten_Test_TTM(int infolevel)
@@ -300,10 +333,5 @@ void Genten_Test_TTM(int infolevel)
     test1();
     test2();
     test3();
-    // testing::InitGoogleTest(&argc, argv);
-    // Kokkos::initialize(argc, argv);
-    // int ret = RUN_ALL_TESTS();
-    // Kokkos::finalize();
-
     finalize();
 }
