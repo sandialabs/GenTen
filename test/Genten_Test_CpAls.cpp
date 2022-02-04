638b0955d998e13cc5c0ac4427eb1af6019768a5
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-02-04 12:47:41 +0100

    Enable HIP for Genten_Test_CpAls

--- test/Genten_Test_CpAls.cpp
+++ test/Genten_Test_CpAls.cpp
@@ -403,6 +403,9 @@ void Genten_Test_CpAls(int infolevel) {
 #ifdef KOKKOS_ENABLE_CUDA
   Genten_Test_CpAls_Space<Kokkos::Cuda>(infolevel);
 #endif
+#ifdef KOKKOS_ENABLE_HIP
+  Genten_Test_CpAls_Space<Kokkos::Experimental::HIP>(infolevel);
+#endif
 #ifdef KOKKOS_ENABLE_OPENMP
   Genten_Test_CpAls_Space<Kokkos::OpenMP>(infolevel);
 #endif
