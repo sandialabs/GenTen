2430dde4491153241bdce725d15512cd81501249
Eric Phipps <etphipp@sandia.gov>
2022-03-09 09:42:42 -0700

    Update caraway build scripts

--- scripts/caraway/do_configuration.sh
+++ scripts/caraway/do_configuration.sh
@@ -9,17 +9,18 @@ rm -rf CMakeFiles
 # so one needs to do a local installation. 
 # Then, ROCM_SEARCH_PATH should be a list of paths pointing to SOLVER, PRIM and Thrust
 # for example for me is:
-#"/ascldap/users/fnrizzi/repos/rocSOLVER/build/release/install;/ascldap/users/fnrizzi/repos/rocPRIM/install;/ascldap/users/fnrizzi/repos/rocThrust/install" \
+export ROCM_SEARCH_PATH="/ascldap/users/fnrizzi/repos/rocSOLVER/build/release/install;/ascldap/users/fnrizzi/repos/rocPRIM/install;/ascldap/users/fnrizzi/repos/rocThrust/install" \
 
 cmake \
     -D CMAKE_CXX_COMPILER=hipcc \
-    -D CMAKE_VERBOSE_MAKEFILE=On \
-    -D CMAKE_CXX_STANDARD=14 \
+    -D CMAKE_VERBOSE_MAKEFILE=OFF \
     -D CMAKE_CXX_FLAGS="-g" \
     -D CMAKE_C_COMPILER=hipcc \
     -D CMAKE_C_FLAGS="-g" \
     -D Kokkos_ENABLE_HIP=ON \
     -D BUILD_SHARED_LIBS=ON \
     -D Kokkos_ARCH_VEGA908=ON \
-    -D ROCM_SEARCH_PATH=<see_note_above>\
-    ..
+    -D ROCM_SEARCH_PATH=${ROCM_SEARCH_PATH} \
+    -D LAPACK_LIBS=$MKL_ROOT/lib/intel64/libmkl_rt.so \
+    -D LAPACK_ADD_LIBS="-lpthread;-lm;-ldl" \
+    ../../genten
