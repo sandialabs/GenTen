d843dbe144ba0b242f42181b309509b67e89babe
etphipp <etphipp@sandia.gov>
2025-06-23 11:47:17 -0600

    Check if directory exists before adding to dll search path

--- python/pygenten/__init__.py
+++ python/pygenten/__init__.py
@@ -20,7 +20,8 @@ if hasattr(os, 'add_dll_directory'):
   if 'GENTEN_DLL_PATH' in os.environ:
     gt_dlls = os.environ['GENTEN_DLL_PATH']
     for dll in gt_dlls.split(';'):
-      os.add_dll_directory(dll)
+      if os.path.isdir(dll):
+        os.add_dll_directory(dll)
 
 from pygenten._pygenten import *
 from pygenten.solvers import cp_als, cp_opt, gcp_opt, gcp_sgd, gcp_fed, online_gcp
