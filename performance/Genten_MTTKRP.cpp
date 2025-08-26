e963fe21d37fd43953c7acdd5764fbf589b520bb
Gabriel Kosmacher <73120774+kennykos@users.noreply.github.com>
2025-08-26 11:33:53 -0600

    Add NVTX ranges to dense kernel call as well

--- performance/Genten_MTTKRP.cpp
+++ performance/Genten_MTTKRP.cpp
@@ -419,6 +419,13 @@ int run_dense_mttkrp(const std::string& inputfilename,
       timer.stop(1+n);
     }
   }
+	#ifdef HAVE_CUDA
+	nvtxRangePushA("MTTKRP");
+	for (ttb_indx n=0; n<nDims; ++n) {
+		Genten::mttkrp(cData, cInput, n, cResult[n], algParams);
+	}
+	nvtxRangePop();
+	#endif
   const double atomic = 1.0; // cost of atomic measured in flops
   const double mttkrp_flops = cData.numel()*nNumComponents*(nDims+atomic);
   double mttkrp_total_time = 0.0;
